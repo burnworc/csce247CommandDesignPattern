1e9109f787a03fce1dc9b347fc8a8e401ceb1b99
burnworc <burnworc@email.sc.edu>
2020-10-05 15:41:20 -0400

    Setting up SellStockCommand class

---
+++ CommandDesignPattern/src/SellStockCommand.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @author 
+ *
+ */
+public class SellStockCommand {
+	private StockTrade stockTrade;
+	private String stockName;
+	private int shares;
+	
+	public SellStockCommand(StockTrade stockTrade, String stockName, int shares) {
+		
+	}
+	
+	public void execute() {
+		
+	}
+}
