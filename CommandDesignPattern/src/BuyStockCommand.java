ad8b97d67144d6019ba0b8bfdfefffe2323c089c
burnworc <burnworc@email.sc.edu>
2020-10-05 15:45:04 -0400

    Setting up BuyStockCommand class

---
+++ CommandDesignPattern/src/BuyStockCommand.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @author 
+ *
+ */
+public class BuyStockCommand {
+	private StockTrade stockTrade;
+	private String stockName;
+	private int shares;
+	
+	public BuyStockCommand(StockTrade stockTrade, String stockName, int shares) {
+		
+	}
+	
+	public void execute() {
+		
+	}
+}
