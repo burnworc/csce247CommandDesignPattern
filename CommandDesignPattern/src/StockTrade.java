8ad340d76b60f9b91ee519165efce0fe051afd83
burnworc <burnworc@email.sc.edu>
2020-10-05 15:34:20 -0400

    Setting up StockTrade

---
+++ CommandDesignPattern/src/StockTrade.java
@@ -0,0 +1,12 @@
+
+public class StockTrade {
+	public StockTrade() {}
+	
+	public void buy(String stockName, int numShares) {
+		
+	}
+	
+	public void sell(String stockName, int numShares) {
+		
+	}
+}
