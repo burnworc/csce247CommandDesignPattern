882a718ba5ffb41a1c99160ce210643932975218
burnworc <burnworc@email.sc.edu>
2020-10-05 16:14:45 -0400

    Made changes to StockTrade

--- CommandDesignPattern/src/StockTrade.java
+++ CommandDesignPattern/src/StockTrade.java
@@ -7,10 +7,10 @@ public class StockTrade {
 	public StockTrade() {}
 	
 	public void buy(String stockName, int numShares) {
-		
+		System.out.println("Buying " + numShares + " of" + stockName);
 	}
 	
 	public void sell(String stockName, int numShares) {
-		
+		System.out.println("Selling " + numShares + " of" + stockName);
 	}
 }
