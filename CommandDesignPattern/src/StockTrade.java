8233ee7f932d2b8f624cca22ced81b26432e39f0
burnworc <70215302+burnworc@users.noreply.github.com>
2020-10-07 14:36:41 -0400

    Update StockTrade.java

--- CommandDesignPattern/src/StockTrade.java
+++ CommandDesignPattern/src/StockTrade.java
@@ -1,9 +1,5 @@
 /**
-<<<<<<< HEAD
  * Holds the methods for displaying messages based on user choice
-=======
- * 
->>>>>>> d7dc55800405dbb3b9c8ee808cc0df6351dea7f8
  * @author Cole Burnworth
  *
  */
