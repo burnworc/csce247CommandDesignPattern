2e9109c27bb0e12190fb5896903c5123a6345867
burnworc <burnworc@email.sc.edu>
2020-10-07 14:26:08 -0400

    Finished the Agent class

--- CommandDesignPattern/src/Agent.java
+++ CommandDesignPattern/src/Agent.java
@@ -1,5 +1,5 @@
 /**
- * 
+ * Holds the arraylist of Commands called orders and all the methods for the arraylist looping.
  * @author Cole Burnworth
  *
  */
@@ -8,7 +8,7 @@ public class Agent {
 	private ArrayList<Command> orders;
 	
 	/**
-	 * Initializes the array
+	 * Initializes the array in the class constructor
 	 */
 	public Agent() {
 		orders = new ArrayList<Command>(); 
@@ -16,14 +16,14 @@ public class Agent {
 	
 	/**
 	 * The command is added to the array list of orders
-	 * @param command
+	 * @param command - Passes in the command that the user selects.
 	 */
 	public void placeOrder(Command command) {
 		orders.add(command);
 	}
 	
 	/**
-	 * Loops through the list of orders and executes each one
+	 * Loops through the list of orders and executes each one with a for loop
 	 */
 	public void processOrders() {
 		for (Command order : orders) {
