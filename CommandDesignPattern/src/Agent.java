5d856c70125f282a7ecdaedd2c74e8dae1c76a20
burnworc <burnworc@email.sc.edu>
2020-10-06 20:04:47 -0400

    Finishing agent class

--- CommandDesignPattern/src/Agent.java
+++ CommandDesignPattern/src/Agent.java
@@ -7,14 +7,24 @@ import java.util.*;
 public class Agent {
 	private ArrayList<Command> orders;
 	
+	/**
+	 * Initializes the array
+	 */
 	public Agent() {
 		orders = new ArrayList<Command>(); 
 	}
 	
+	/**
+	 * The command is added to the array list of orders
+	 * @param command
+	 */
 	public void placeOrder(Command command) {
 		orders.add(command);
 	}
 	
+	/**
+	 * Loops through the list of orders and executes each one
+	 */
 	public void processOrders() {
 		for (Command order : orders) {
 			order.execute();
