1c3620dedab5892e684ec54c5662ec8e0dde0b85
yercap <67244120+yercap@users.noreply.github.com>
2020-10-08 00:15:02 -0400

    Finishing constructor and execute methods

--- CommandDesignPattern/src/BuyStockCommand.java
+++ CommandDesignPattern/src/BuyStockCommand.java
@@ -8,11 +8,22 @@ public class BuyStockCommand implements Command {
 	private String stockName;
 	private int shares;
 	
+	/**
+	 * Simple constructor that sets attributes for the trade
+	 * @param stockTrade
+	 * @param stockName
+	 * @param shares
+	 */
 	public BuyStockCommand(StockTrade stockTrade, String stockName, int shares) {
-		
+		this.stockTrade = stockTrade;
+		this.stockName = stockName;
+		this.shares = shares;	
 	}
 	
+	/**
+	 * Calls upon the stockTrade class method buy() to purchase a stock and the amount based on the parameters
+	 */
 	public void execute() {
-		
+		stockTrade.buy(stockName, shares);
 	}
 }
