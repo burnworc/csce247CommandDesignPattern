18fe67815dbad68d1a57e252461831403de9d2e0
jbb1209 <65250598+jbb1209@users.noreply.github.com>
2020-10-08 00:35:21 -0400

    selling code

--- CommandDesignPattern/src/BuyStockCommand.java
+++ CommandDesignPattern/src/BuyStockCommand.java
@@ -8,6 +8,7 @@ public class BuyStockCommand implements Command {
 	private String stockName;
 	private int shares;
 	
+	
 	public BuyStockCommand(StockTrade stockTrade, String stockName, int shares) {
 		
 	}
--- CommandDesignPattern/src/SellStockCommand.java
+++ CommandDesignPattern/src/SellStockCommand.java
@@ -8,11 +8,26 @@ public class SellStockCommand implements Command {
 	private String stockName;
 	private int shares;
 	
+	/**
+	 * 
+	 * @param stockTrade
+	 * @param stockName
+	 * @param shares
+	 */
+	
 	public SellStockCommand(StockTrade stockTrade, String stockName, int shares) {
+		this.stockTrade = stockTrade;
+		this.stockName = stockName;
+		this.shares = shares;
 		
 	}
+	/**
+	 * executes the selling of stock
+	 */
 	
 	public void execute() {
+		
+		stockTrade.sell(stockName,shares);
 		
 	}
 }
